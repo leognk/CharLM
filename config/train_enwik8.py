393a644d1d6fd2e45792dec95e07f211e8e522d1
leognk <leotaisei@gmail.com>
2024-04-10 13:37:45 +0000

    updated

--- config/train_enwik8.py
+++ config/train_enwik8.py
@@ -1,33 +1,34 @@
-out_dir = 'exp/out-enwik8'
-eval_interval = 250 # keep frequent because we'll overfit
+name = 'gpt_1'
+
+out_dir = f'exp/enwik8/{name}'
+eval_interval = 500
 eval_iters = 200
-log_interval = 10 # don't print too too often
+log_interval = 10
 
-# we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
-wandb_log = True # override via command line if you like
+init_from = 'scratch'
+
+wandb_log = True
 wandb_project = 'enwik8'
-wandb_run_name = 'mini-gpt'
+wandb_run_name = name
 
 dataset = 'enwik8'
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 60
+block_size = 1024
 
-# baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
-dropout = 0.2
+n_layer = 12
+n_head = 12
+n_embd = 768
+dropout = 0.0
 
-learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-4 # learning_rate / 10 usually
-beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+max_iters = 50000
+lr_decay_iters = max_iters
 
-warmup_iters = 100 # not super necessary potentially
+learning_rate = 6e-4
+min_lr = learning_rate / 10
 
-# device = 'cpu'  # run on cpu only
-# compile = False # do not torch compile the model
+warmup_iters = 500
+
+# compile = False
