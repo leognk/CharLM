8614160e12cfee4a6595912574b853cc89861e24
leognk <leotaisei@gmail.com>
2024-04-10 12:28:18 +0000

    updated

--- config/sample_enwik8.py
+++ config/sample_enwik8.py
@@ -1,7 +1,10 @@
+data_in_bytes = True
+
 init_from = 'resume'
 out_dir = 'exp/out-enwik8'
 
-start = "\n"
+# start = "\n"
+start = "What is the answer to life, the universe, and everything?"
 
 num_samples = 5
 max_new_tokens = 500
