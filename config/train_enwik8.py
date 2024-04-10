6899afc13bbab607239bea24998d51cdc4b334c6
leognk <leotaisei@gmail.com>
2024-04-10 13:46:22 +0000

    updated

--- config/train_enwik8.py
+++ config/train_enwik8.py
@@ -1,7 +1,7 @@
 name = 'gpt_1'
 
 out_dir = f'exp/enwik8/{name}'
-eval_interval = 500
+eval_interval = 1000
 eval_iters = 200
 log_interval = 10
 
@@ -15,20 +15,20 @@ wandb_run_name = name
 
 dataset = 'enwik8'
 gradient_accumulation_steps = 1
-batch_size = 60
+batch_size = 64
 block_size = 1024
 
 n_layer = 12
-n_head = 12
-n_embd = 768
+n_head = 16
+n_embd = 512
 dropout = 0.0
 
-max_iters = 50000
+max_iters = 100000
 lr_decay_iters = max_iters
 
 learning_rate = 6e-4
 min_lr = learning_rate / 10
 
-warmup_iters = 500
+warmup_iters = 1000
 
 # compile = False
