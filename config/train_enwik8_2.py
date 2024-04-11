2193474502e89ffc662273f8cc8e32713ad9f9a9
leognk <leotaisei@gmail.com>
2024-04-11 12:48:46 +0000

    init

---
+++ config/train_enwik8_2.py
@@ -0,0 +1,38 @@
+name = 'gpt'
+
+out_dir = f'exp/enwik8/{name}'
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+always_save_checkpoint = False
+
+init_from = 'scratch'
+
+wandb_log = True
+wandb_project = 'enwik8'
+wandb_run_name = name
+
+dataset = 'enwik8'
+gradient_accumulation_steps = 1
+batch_size = 16
+block_size = 1024
+
+n_layer = 12
+n_head = 16
+n_embd = 512
+dropout = 0.2
+
+max_iters = 200000
+lr_decay_iters = max_iters
+
+learning_rate = 1e-3
+min_lr = 1e-4
+
+warmup_iters = 1000
+
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+
+# compile = False
