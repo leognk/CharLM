c8ece372cadd1e5e202eb695b07d1543b3e1f64e
leognk <leotaisei@gmail.com>
2024-04-12 12:30:47 +0000

    init

---
+++ config/sample_enwik8_2.py
@@ -0,0 +1,15 @@
+name = 'patch_4'
+
+init_from = 'resume'
+out_dir = f'exp/enwik8/{name}'
+
+data_in_bytes = True
+
+start = "\n"
+# start = "What is the answer to life, the universe, and everything?"
+
+num_samples = 5
+max_new_tokens = 500
+temperature = 0.8
+
+# device = 'cpu'
