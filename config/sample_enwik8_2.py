07cc577a21c6acaf0c529d68d1496370ef1060da
leognk <leotaisei@gmail.com>
2024-04-12 13:57:40 +0000

    updated

--- config/sample_enwik8_2.py
+++ config/sample_enwik8_2.py
@@ -5,11 +5,11 @@ out_dir = f'exp/enwik8/{name}'
 
 data_in_bytes = True
 
-start = "\n"
-# start = "What is the answer to life, the universe, and everything?"
+# start = "\n"
+start = "What is the answer to life, the universe, and everything?"
 
 num_samples = 5
-max_new_tokens = 500
+max_new_tokens = 2048
 temperature = 0.8
 
 # device = 'cpu'
