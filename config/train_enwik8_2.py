857fcd57d0569cd8431386a8497a430a20f03e17
leognk <leotaisei@gmail.com>
2024-04-12 14:09:26 +0000

    updated

--- config/train_enwik8_2.py
+++ config/train_enwik8_2.py
@@ -1,7 +1,7 @@
-name = 'patch_1'
+name = 'patch'
 
 out_dir = f'exp/enwik8/{name}'
-eval_interval = 250
+eval_interval = 1000
 eval_iters = 200
 log_interval = 10
 
@@ -16,27 +16,27 @@ wandb_run_name = name
 dataset = 'enwik8'
 
 sub_block_size = 1024
-patch_size = 8
+patch_size = 2
 block_size = patch_size * sub_block_size
 
 gradient_accumulation_steps = 1
-batch_size = 64 // patch_size
+batch_size = 16 // patch_size
 
-n_layer = 5
-n_head = 5
-n_embd = 320
+n_layer = 10
+n_head = 16
+n_embd = 512
 dropout = 0.2
 
-max_iters = 5000
+max_iters = 200000
 lr_decay_iters = max_iters
 
 learning_rate = 1e-3
 min_lr = 1e-4
 
-warmup_iters = 100
+warmup_iters = 1000
 
 weight_decay = 1e-1
 beta1 = 0.9
-beta2 = 0.99
+beta2 = 0.95
 
 # compile = False
